bf02cc6ce8ae2fde0144ab9701f4ef8c2d866a3b
fabiocst <fabiocos996@gmail.com>
2024-05-30 00:40:07 -0300

    middleware parcialmente funcional

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const rateLimit = require('express-rate-limit');
 //protocolo de comunicacão entre apis e outros serviços cors
 //CORS: autoriza para qualquer tipo de serviço (front-end, outras
 //apis, etc)
 const cors = require('cors')
 const app = express();
+
+let limiter = rateLimit({
+    max: 3,
+    windowMs: 60*60*1000,
+    message:'We have received too many requests from this IP'
+});
+
+app.use('/', limiter);
+
+
 const port = 8081;
 //importações
 const proprietario = require('./controllers/ProprietarioControlls.js');
